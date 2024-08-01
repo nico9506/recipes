b369e43df4dc5ad8dd4fde1f485742dae5233916
kevinleo1996f <kevinleo1996f@gmail.com>
2024-08-01 15:23:07 +1000

    update API request with POST input and GET statuscode

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const https = require('node:https');
 const express = require('express');
+const { request } = require('node:http');
 const app = express();
 const port = 3000;
 
@@ -12,7 +14,31 @@ app.get('/', (req,res)=>{
 app.post('/input', (req,res)=>{
     const words = (req.body.word);
     console.log(words);
-})
+    const link = {
+        hostname: 'api.api-ninjas.com',
+        path: `https://api.api-ninjas.com/v1/recipe?query=${words}`,
+        headers: {'X-Api-Key': '+6dQsRgnJJn2Fb/lPVdDmg==OIPaS5RXB0pvor13'}
+    }
+    console.log(link)
+    https.get(link, (resp)=>{
+        console.log(resp.statusCode)
+        // let data = ''
+        // resp.on('data', (chunk)=>{
+        //     data += chunk;
+        // })
+
+        // resp.on('end', () => {
+        //     console.log(JSON.parse(data));
+        //     res.send(JSON.parse(data));
+        // });
+
+    //     // console.log(data[0])
+    // }).on('error', (err) => {
+    //     console.log('Error: ' + err.message);
+    //     res.status(500).send('Error retrieving data from API');
+     });
+});
+    
 
 app.listen(port, ()=>{
     console.log(`your server run in localhost:${port}`);
